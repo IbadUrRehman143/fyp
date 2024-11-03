c72dacfc50477d1b274395da5e18fc7abd6950f9
Gulalam <khangulalam.k@gmail.com>
2024-11-03 17:23:53 +0500

    small changes

--- doctor/settings.php
+++ doctor/settings.php
@@ -283,7 +283,7 @@
                         <h2></h2>
                         <a class="close" href="settings.php">&times;</a>
                         <div class="content">
-                            eDoc Web App<br>
+                            docnest Web App<br>
                             
                         </div>
                         <div style="display: flex;justify-content: center;">
